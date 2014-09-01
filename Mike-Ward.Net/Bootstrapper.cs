980303ebdd63c410218ba9ca9738f96b99627db0
Mike Ward <mike@mike-ward.net>
2014-09-01 16:08:42 -0400

    Add highlightjs

--- Mike-Ward.Net/Bootstrapper.cs
+++ Mike-Ward.Net/Bootstrapper.cs
@@ -1,5 +1,9 @@
-﻿using Nancy.Conventions;
+﻿using System;
+using Nancy.Blog;
+using Nancy.Bootstrapper;
+using Nancy.Conventions;
 using Nancy.Pile;
+using Nancy.TinyIoc;
 
 namespace Mike_Ward.Net
 {
@@ -7,6 +11,22 @@ namespace Mike_Ward.Net
 
     public class Bootstrapper : DefaultNancyBootstrapper
     {
+        protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
+        {
+            base.ApplicationStartup(container, pipelines);
+
+            var blog = new Blog
+            {
+                Title = "Mike-Ward.Net",
+                Description = ".NET, Technology, Life, Whatever",
+                BaseUri = new Uri("http://localhost:12116/blog"),
+                Author = "Mike Ward",
+                Langauge = "en-US",
+                Copyright = "Copyright (C) 2014 - Mike Ward"
+            };
+            container.Register<IBlog>(blog);
+        }
+
         protected override void ConfigureConventions(NancyConventions nancyConventions)
         {
             base.ConfigureConventions(nancyConventions);
@@ -14,7 +34,14 @@ namespace Mike_Ward.Net
             nancyConventions.StaticContentsConventions.StyleBundle("styles.css",
                 new []
                 {
-                    "css/pure-min.css"
+                    "css/pure-min.css",
+                    "css/*.css"
+                });
+
+            nancyConventions.StaticContentsConventions.ScriptBundle("scripts.js",
+                new []
+                {
+                    "*.js"
                 });
         }
     }
--- Mike-Ward.Net/Modules/BlogModule.cs
+++ Mike-Ward.Net/Modules/BlogModule.cs
@@ -1,10 +1,11 @@
 ﻿using Nancy;
+using Nancy.Blog;
 
 namespace Mike_Ward.Net.Modules
 {
     public class BlogModule : NancyModule
     {
-        public BlogModule()
+        public BlogModule(IBlog blog)
         {
             Get["/blog"] = p => View["blog"];
         }
