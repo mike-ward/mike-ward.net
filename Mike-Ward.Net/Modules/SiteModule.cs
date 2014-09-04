a3ee8b8401fb192273240f7966a4d37a91acc950
Mike Ward <mike@mike-ward.net>
2014-09-03 22:48:40 -0400

    Add deskdrive, tweetz, simplyweather pages.

--- Mike-Ward.Net/Modules/SiteModule.cs
+++ Mike-Ward.Net/Modules/SiteModule.cs
@@ -7,7 +7,9 @@
         public SiteModule()
         {
             Get["/"] = p => Response.AsRedirect("~/blog");
+            Get["/deskdrive"] = p => View["deskdrive"];
             Get["/tweetz"] = p => View["tweetz"];
+            Get["/simplyweather"] = p => View["simplyweather"];
             Get["/vscoloroutput"] = p => View["vscoloroutput"];
         }
     }
