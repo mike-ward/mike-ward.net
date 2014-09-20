921283f218166df91185aaf596fc8ad0a41a6e07
mike-ward <mike@mike-ward.net>
2014-09-20 13:04:20 -0400

    Wire debug/release relative urls

--- Mike-Ward.Net/Modules/SiteModule.cs
+++ Mike-Ward.Net/Modules/SiteModule.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.Ajax.Utilities;
-using Nancy.Responses;
+﻿using Nancy.Responses;
+using Nancy.Responses.Negotiation;
 
 namespace Mike_Ward.Net
 {
@@ -9,25 +9,33 @@ namespace Mike_Ward.Net
     {
         public SiteModule()
         {
-            Get["/deskdrive"] = p => View["deskdrive"];
-            Get["/tweetz"] = p => View["tweetz"];
-            Get["/tweetz-help"] = p => View["tweetz-help"];
-            Get["/simplyweather"] = p => View["simplyweather"];
-            Get["/vscoloroutput"] = p => View["vscoloroutput"];
-            Get["/freesnap"] = p => View["freesnap"];
-            Get["/calendar"] = p => View["calendar"];
-            Get["/gadgets"] = p => View["gadgets"];
-            Get["/calendargadget"] = p => View["calendargadget"];
-            Get["/downloads"] = p => View["downloads"];
-            Get["/about"] = p => View["about"];
-            Get["/installmonetizer"] = p => View["installmonetizer"];
+            Get["/deskdrive"] = p => CreateView("deskdrive");
+            Get["/tweetz"] = p => CreateView("tweetz");
+            Get["/tweetz-help"] = p => CreateView("tweetz-help");
+            Get["/simplyweather"] = p => CreateView("simplyweather");
+            Get["/vscoloroutput"] = p => CreateView("vscoloroutput");
+            Get["/freesnap"] = p => CreateView("freesnap");
+            Get["/calendar"] = p => CreateView("calendar");
+            Get["/gadgets"] = p => CreateView("gadgets");
+            Get["/calendargadget"] = p => CreateView("calendargadget");
+            Get["/downloads"] = p => CreateView("downloads");
+            Get["/about"] = p => CreateView("about");
+            Get["/installmonetizer"] = p => CreateView("installmonetizer");
             Get["/robots.txt"] = p => new GenericFileResponse("robots.txt");
 
             Get["/download/{cg?}/{file}"] = p =>
             {
                 ViewBag.File = (p.cg != null) ? p.cg + "/" + p.file : p.file;
-                return View["download"];
+                return CreateView("download");
             };
+        }
+
+        private Negotiator CreateView(string viewName)
+        {
+#if !DEBUG
+            ViewBag.BaseCdnUri = "http://az667460.vo.msecnd.net/";
+#endif
+            return View[viewName];
         }
     }
 }
